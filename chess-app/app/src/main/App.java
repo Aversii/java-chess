de442f9c3b5bddf8d699c2fcbc38fbdeb629e15b
aversii <lucasaversi@gmail.com>
2023-05-22 00:21:10 -0300

    class position created and working

--- chess-app/app/src/main/App.java
+++ chess-app/app/src/main/App.java
@@ -1,7 +1,10 @@
-package App;
+package main;
+
+import boargame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
