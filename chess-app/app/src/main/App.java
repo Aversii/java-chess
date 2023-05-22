5c5d95c5245eff6fb379834c976894afbe735aaa
aversii <lucasaversi@gmail.com>
2023-05-22 00:51:28 -0300

    starting implementing board and piece classes

--- chess-app/app/src/main/App.java
+++ chess-app/app/src/main/App.java
@@ -1,10 +1,11 @@
 package main;
 
+import boargame.Board;
 import boargame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
