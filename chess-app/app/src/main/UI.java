680846226aaa0dc76fecda0ed0afb871f1da96c3
aversii <lucasaversi@gmail.com>
2023-05-25 17:11:06 -0300

    pieces now are printed in correct collor

--- chess-app/app/src/main/UI.java
+++ chess-app/app/src/main/UI.java
@@ -1,8 +1,20 @@
  package main;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
 
     public static void printBoard(ChessPiece[][] pieces){
         for (int i = 0; i<pieces.length; i++){
@@ -21,7 +33,11 @@ public class UI {
             System.out.print("-");
         }
         else{
-            System.out.print(piece);
+            if (piece.getColor() == Color.WHITE){
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }else if(piece.getColor()==Color.BLACK){
+                System.out.print(ANSI_BLACK + piece + ANSI_RESET);
+            }
         }
         System.out.print(" ");
     }
