501d5bc08ac4ec3a99706042458c4d14bbd32876
aversii <lucasaversi@gmail.com>
2023-05-31 18:48:55 -0300

    Kings movements implemented, now kings can move and capture

--- chess-app/app/src/chess/pieces/King.java
+++ chess-app/app/src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boargame.Board;
+import boargame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,7 +20,103 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRow()][getBoard().getRow()];
+        Position auxPosition = new Position(0, 0);
+
+
+        // above moves
+        auxPosition.setValues(position.getRow() - 1, position.getColumn());
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // down moves
+        auxPosition.setValues(position.getRow() + 1, position.getColumn());
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // left moves
+        auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // right moves
+        auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // diagonal up-left moves
+
+        auxPosition.setValues(position.getRow() - 1, position.getColumn()-1);
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+
+        // diagonal up-right moves
+
+        auxPosition.setValues(position.getRow() - 1, position.getColumn()+  1);
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }        
+
+        // diagonal bottom-right moves
+
+        auxPosition.setValues(position.getRow() + 1, position.getColumn()+1);
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // diagonal bottom-left moves
+        
+        auxPosition.setValues(position.getRow() + 1, position.getColumn()-1);
+        if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+
+        }
+
+        if (getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) {
+            mat[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+        
         return mat;
     }
+
+    
     
 }
